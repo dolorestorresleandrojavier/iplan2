125076c553646669b4bfd8f9f53130bd2f7e3af0
dolorestorresleandrojavier <dolorestorresleandrojavier@gmail.com>
2025-04-18 21:27:33 -0300

    4

---
+++ themes/iplan_b4/js/global8fd4.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.iplan_b4 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
